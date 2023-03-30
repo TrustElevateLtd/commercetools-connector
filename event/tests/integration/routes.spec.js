4e6c49b26a9f3c6e858e471490fc62bd11815a60
Dennis Gloss <dennisgloss23@gmail.com>
2023-03-30 10:37:19 +0200

    tidy up

--- event/tests/integration/routes.spec.js
+++
@@ -1,7 +0,0 @@
-//@todo: test this with msw (see typescipt job)
-//  make msw a shared package
-describe('Testing Event Controller', () => {
-  test('POST `/event` route', async () => {
-    expect(1).toBe(1);
-  });
-});
--- trustelev8service/tests/integration/routes.spec.js
+++ trustelev8service/tests/integration/routes.spec.js
@@ -1,6 +1,6 @@
 //@todo: implement tests (use msw in shared package)
 describe('Testing Cart Controller', () => {
-  test('POST `/service` route', async () => {
+  test('POST `/trustelev8service` route', async () => {
     expect(1).toBe(1);
   });
 });
