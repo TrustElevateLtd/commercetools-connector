021f74b993d9b93cfad17ab6d17b79801d3f0087
Dennis Gloss <dennisgloss23@gmail.com>
2023-05-04 15:54:39 +0200

    add error test

--- trustelev8service/tests/unit/controllers/service.controller.js
+++ trustelev8service/tests/unit/controllers/service.controller.js
@@ -1,3 +1,5 @@
+import CustomError from "../../../src/errors/custom.error.js";
+
 jest.mock('../../../src/client/create.client.js', () => ({
     createApiRoot: jest.fn(() => ({
         carts: jest.fn(() => ({
@@ -12,6 +14,8 @@ jest.mock('../../../src/client/create.client.js', () => ({
 }));
 
 import { post } from '../../../src/controllers/service.controller.js';
+import { createApiRoot } from '../../../src/client/create.client.js';
+
 
 describe('post', () => {
     it('should return 200', async () => {
@@ -28,5 +32,33 @@ describe('post', () => {
         expect(res.status.mock.calls.length).toBe(1);
         expect(res.status.mock.calls[0][0]).toBe(200);
         expect(res.json.mock.calls.length).toBe(1);
+    });
+    it('should return 500 if server fails', async () => {
+        createApiRoot.mockImplementation(jest.fn(() => ({
+            carts: jest.fn(() => ({
+                withId: jest.fn(() => ({
+                    post: jest.fn(() => ({
+                        statusCode: 401,
+                        actions: [{ action: 'setCustomField', name: 'eligible', value: true }],
+                        message: "Unauthenticated"
+                    })),
+                })),
+            })),
+        })))
+        expect.assertions(1);
+        const req = {body: {id: 1}}
+        const res = {
+            status: jest.fn(() => res),
+            send: jest.fn(),
+            json: jest.fn(),
+            body: {
+                id: 1
+            }
+        };
+        try {
+            await post(req, res);
+        } catch (error) {
+            expect(error).toBeInstanceOf(CustomError);
+        }
     });
 });
